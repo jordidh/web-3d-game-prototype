85c0f5379f4d9f5bd021697a9f9c94406cc39218
jordidh <jordi.dh@gmail.com>
2019-12-17 22:44:57 +0100

    es crea un nou joc a partir d'un exemple per modificar-lo posteriorment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/name', function(req, res, next) {
+  console.log(JSON.stringify(req.body));
+  if (!('name' in req.body)) {
+    return res.render('error', { message: 'gamer name attribute not found in request'});
+  }
+  res.render('gameSelect', { gamer: req.body.name });
+});
+
+router.post('/game', function(req, res, next) {
+  console.log(JSON.stringify(req.body));
+  if (!('name' in req.body)) {
+    return res.render('error', { message: 'gamer name attribute not found in request'});
+  }
+  if (!('game' in req.body)) {
+    return res.render('error', { message: 'game attribute not found in request'});
+  }
+  res.render('game', { gamer: req.body.name, game: req.body.game });
+});
+
+router.get('/warehouse', function(req, res, next) {
+  res.render('warehouse');
 });
 
 module.exports = router;
